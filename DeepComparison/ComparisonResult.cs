09fd65efcda7447c876e4e0b588495191cff7350
Ilya <marhoily@hotmail.com>
2016-08-03 14:26:53 +0300

    + ComparisonResult

---
+++ DeepComparison/ComparisonResult.cs
@@ -0,0 +1,15 @@
+﻿namespace DeepComparison
+{
+    /// <summary>Combines result of comparison with the description of what did not compare </summary>
+    public sealed class ComparisonResult
+    {
+        /// <summary>True if objects are equal</summary>
+        public bool AreEqual { get; }
+        /// <summary>Path to the property that does not compare</summary>
+        public string Path { get; }
+        /// <summary>ctor</summary>
+        public ComparisonResult(bool areEqual) { AreEqual = areEqual; }
+        /// <summary>ctor</summary>
+        public ComparisonResult(string path) { Path = path; }
+    }
+}
