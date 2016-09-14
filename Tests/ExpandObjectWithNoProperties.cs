1469263e009bd6b8a1e6a9445755411270905f7a
Ilya <marhoily@hotmail.com>
2016-09-14 17:17:34 +0300

    ExpandObjectWithNoProperties

---
+++ Tests/ExpandObjectWithNoProperties.cs
@@ -0,0 +1,27 @@
+﻿using DeepComparison;
+using FluentAssertions;
+using Xunit;
+using static DeepComparison.ComparisonResult;
+
+namespace Tests
+{
+    public class ExpandObjectWithNoProperties
+    {
+        private readonly DeepComparer _comparer =
+            new DeepComparerBuilder()
+                .GoDeepFor(t => t == typeof(X))
+                .Build();
+
+        [Fact]
+        public void Should_Equal()
+        {
+            var a = new X();
+            var b = new X();
+            _comparer.Compare(a, b).Should().Be(True);
+        }
+
+        public class X
+        {
+        }
+    }
+}
